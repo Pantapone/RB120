0688d02c81370425a1f01668910e684e8e415755
Victoria Schoeffel <vns28cam.ac.uk>
2021-02-23 20:13:23 +0100

    RPS game added

---
+++ RB120/lesson_2/lecture_classes_objects.rb
@@ -0,0 +1,33 @@
+class Person
+  attr_accessor :first_name, :last_name
+
+  def initialize(full_name)
+    parts = full_name.split
+    @first_name = parts.first
+    @last_name = parts.size > 1 ? parts.last : ""
+  end
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+
+  def name=(full_name)
+    parts = full_name.split
+    self.first_name = parts.first
+    self.last_name = parts.size > 1 ? parts.last : ""
+  end
+
+end
+
+
+bob = Person.new("bob")
+puts bob.name
+bob.name = "Robert"
+puts bob.name
+
+bob = Person.new('Robert')
+bob.name                  # => 'Robert'
+bob.first_name            # => 'Robert'
+bob.last_name             # => ''
+bob.last_name = 'Smith'
+bob.name                  # => 'Robert Smith'
